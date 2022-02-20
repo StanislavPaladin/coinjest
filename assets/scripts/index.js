2d4674d67cd452adc256c2f268894ea53cc3dc5b
Stanislav Paladin <paladinstanis@gmail.com>
2022-02-20 23:03:07 +0300

    пофиксил мелкие баги

---
+++ assets/scripts/index.js
@@ -0,0 +1,15 @@
+$('.index-slider').slick({
+    infinite: true,
+    centerMode: true,
+    dots: false,
+    slidesToShow: 1,
+    autoplay: true,
+    fade: true,
+    autoplaySpeed: 5000,
+    slidesToScroll: 1,
+    slidesPerRow: 1,
+    prevArrow: $('#prev'),
+    nextArrow: $('#next'),
+});
+
+
