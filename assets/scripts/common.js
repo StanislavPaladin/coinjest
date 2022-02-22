81e297e35b3a361d1fa8112ad3d6d1c25fad791b
Stanislav Paladin <paladinstanis@gmail.com>
2022-02-22 15:42:23 +0300

    добавил страницы логина и регистраци

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,2 +1,7 @@
 // burger navigation
-document.querySelector('.header-burger').addEventListener('click', ()=> document.querySelector('.modal-nav').classList.toggle('active'));
+const burger = document.querySelector('.header-burger');
+const cross = document.getElementById('cross');
+const modal = document.querySelector('.modal')
+const togglers = [burger, cross];
+
+togglers.forEach(item => item.addEventListener('click', ()=> modal.classList.toggle('active')));
